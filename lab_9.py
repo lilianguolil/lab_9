51cbfa7906e8128f531c5a3f6194ccf73432dc34
victoriayvu <166648948+victoriayvu@users.noreply.github.com>
2024-04-10 15:34:59 -0400

    swoosh

--- lab_9.py
+++ lab_9.py
@@ -1,3 +1,4 @@
+
 def decoder(num):
     result = ''
     for digit in num:
@@ -5,5 +6,27 @@ def decoder(num):
         result += new_digit
     return result
 
+def start():
+    while True:
+        menu()
+        option = int(input("\nPlease enter an option: "))
+        if option == "1":
+            num = int(input("Please enter your password to encode: "))
+            encode()
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            decoder()
+            print(f"The encoded password is {xxx}, and the original password is {num}.")
+        elif option == "3":
+            break
+def menu():
+    print("\nMenu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    return
 
-d
+
+if __name__ == "__main__":
+    start()
